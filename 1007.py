4077c9563d901cbfe913230fcc5e0ac501ec1b0c
mearexx <danielrepin04@gmail.com>
2025-11-08 18:02:46 +0100

    Add files via upload

---
+++ 1007.py
@@ -0,0 +1,21 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+================ RESTART: C:/Users/user/Documents/пейтон/1006.py ===============
+Country Name | 2018 [YR2018] | 2019 [YR2019]
+Ukraine : 48.9 | 50.2
+Italy : 32.1 | 31.5
+Germany : 46.9 | 47.5
+France : 30.8 | 31.2
+Poland : 55.7 | 56.2
+USA : 12.2 | 11.7
+Spain : 34.4 | 35.1
+Switzerland : 64.3 | 65.0
+Japan : 18.5 | 18.6
+Canada : 30.7 | 31.0
+
+Введіть назви країн через кому (наприклад: Ukraine, Italy, France): 
+USA, Japan
+Результати пошуку:
+USA : 12.2 | 11.7
+Japan : 18.5 | 18.6
